6e4f20011bd7cb1803553b9edc7a9696e30ea0c0
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 22:47:00 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+  root 'problems#index'
+
+  resources 'problems'
+
   get '/login' => 'sessions#login'
   post '/login' => 'sessions#process_login'
+  get '/logout' => 'sessions#logout'
 
   get '/register' => 'users#new'
   post '/register' => 'users#create'
+
+  get '/problem' => 'problems#new'
+  post '/problem' => 'problems#create'
 
 end
