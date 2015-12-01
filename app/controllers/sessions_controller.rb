3bb6d39939bc77928ef4669ec96b8e3ef7e97e46
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 19:37:07 +0700

    tambah login dan process login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+  def login
+
+  end
+
+  def process_login
+    @user = User.authenticate(params[:email], params[:password])
+    if @user && @user.enable == true && @user.role == "ROLE_USER"
+      flash[:notice] = "Anda berhasil login"
+      redirect_to "/"
+    else
+      flash[:danger] = "username dan password masih salah"
+      render "login"
+    end
+  end
+
 end
