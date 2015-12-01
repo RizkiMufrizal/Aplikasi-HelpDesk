c3afd9c77b7b39472c2a2515815a8e8a644bb690
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 22:47:23 +0700

    update logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,23 @@ class SessionsController < ApplicationController
     @user = User.authenticate(params[:email], params[:password])
     if @user && @user.enable == true && @user.role == "ROLE_USER"
       flash[:notice] = "Anda berhasil login"
+      session[:user_name] = @user.name
+      session[:user_id] = @user.id
+      session[:user_email] = @user.email
+      session[:user_login] = true
       redirect_to "/"
     else
       flash[:danger] = "username dan password masih salah"
       render "login"
     end
+  end
+
+  def logout
+    flash[:notice] = "Anda berhasil logout"
+    session[:user_name] = nil
+    session[:user_email] = nil
+    session[:user_login] = false
+    redirect_to action: "login"
   end
 
 end
