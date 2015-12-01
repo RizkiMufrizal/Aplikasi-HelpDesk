ed8b59e7c978765e45ac3ee2c88daa4c3fa9e594
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 22:47:13 +0700

    update problem

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,2 +1,26 @@
 class ProblemsController < ApplicationController
+
+  def index
+
+    if session[:user_login]
+      @problems = Problem.where(user_id: session[:user_id]).paginate(:page => params[:page], :per_page => 10)
+    else
+      redirect_to "/login"
+    end
+
+  end
+
+  def new
+    @problem = Problem.new
+  end
+
+  def create
+    @problem = Problem.new(problem_params)
+  end
+
+  private
+    def problem_params
+      params.require(:problem).permit(:title, :description, :picture, :user_id)
+    end
+
 end
