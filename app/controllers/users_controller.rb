c6e481ee71d3214452e4486703e54a877de43d4e
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 19:37:19 +0700

    tambah register user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.valid?
+      @user.save
+      flash[:notice] = "Anda berhasil melakukan register, silahkan login"
+      redirect_to "/login"
+    else
+      render "new"
+    end
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password_hash)
+    end
+
 end
