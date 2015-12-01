db11eca70ca621b513865bb640478dcb405df517
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-01 22:50:16 +0700

    set status default

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,11 @@
 class Problem < ActiveRecord::Base
+
+  before_save :set_status
+  validates :title, :description, :picture, :user_id, presence: true
+
+  private
+    def set_status
+      self.status = 'sedang dikerjakan'
+    end
+
 end
