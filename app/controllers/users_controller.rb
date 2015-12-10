38f646f91794829074cbf586b0c5630fe35de6f6
RizkiMufrizal <mufrizalrizki@gmail.com>
2015-12-10 11:55:48 +0700

    update verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,22 @@ class UsersController < ApplicationController
 
     if @user.valid?
       @user.save
-      flash[:notice] = "Anda berhasil melakukan register, silahkan login"
+      UserNotification.send_verification_email(@user).deliver
+      flash[:notice] = "Anda berhasil melakukan register, silahkan lakukan verifikasi email anda"
       redirect_to "/login"
     else
       render "new"
     end
 
+  end
+
+  def verification
+      @user = User.find(params[:id])
+      @user.enable = true
+      if @user.save
+        flash[:notice] = "Anda berhasil melakukan verifikasi, silahkan login"
+        redirect_to "/login"
+      end
   end
 
   private
